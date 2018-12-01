b0d652df4ea46ef20717194e9b32854626aece5c
gEOIS44 <g301244@gmail.com>
2018-12-01 12:37:19 +0900

    初版リリース

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'users_text_search_module_initializer'
+
+Redmine::Plugin.register :redmine_users_text_search do
+  name 'Redmine Users Text Search plugin'
+  author 'g301244'
+  description 'This plug-in searches for the user information by a text.'
+  version '0.1.0'
+  url 'https://github.com/g301244/redmine_users_text_search'
+  author_url 'https://github.com/g301244'
+end
