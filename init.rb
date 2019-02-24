2ce222e687e25893d517fe4d8931b51589002a5a
gEOIS44 <g301244@gmail.com>
2019-02-24 23:49:32 +0900

    制限事項の記載を修正、ほか

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require 'users_text_search_module_initializer'
 
 Redmine::Plugin.register :redmine_users_text_search do
   name 'Redmine Users Text Search plugin'
-  author 'g301244'
+  author 'gEOIS44'
   description 'This plug-in searches for the user information by a text.'
   version '0.2.1'
-  url 'https://github.com/g301244/redmine_users_text_search'
-  author_url 'https://github.com/g301244'
+  url 'https://github.com/gEOIS44/redmine_users_text_search'
+  author_url 'https://github.com/gEOIS44'
 end
